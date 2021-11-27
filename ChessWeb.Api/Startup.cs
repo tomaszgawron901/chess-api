a26393d3c26826891f9eb60a0e47e87306166ff0
tomaszgawron901 <mrgawron901@gmail.com>
2021-11-27 17:45:22 +0100

    fixed services

--- ChessWeb.Api/Startup.cs
+++ ChessWeb.Api/Startup.cs
@@ -2,18 +2,14 @@ using ChessWeb.Api.Hubs;
 using ChessWeb.Api.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.ResponseCompression;
-using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 
 namespace ChessWeb.Api
 {
@@ -44,7 +40,7 @@ namespace ChessWeb.Api
             services.AddSingleton<GameService>();
             services.AddSingleton<ConnectionToRoomService>();
             services.AddControllers();
-            services.AddSignalR().AddAzureSignalR(Configuration["AZURESIGNALR:CONNECTIONSTRING"]);
+            services.AddSignalR().AddAzureSignalR();
             services.AddResponseCompression(opts =>
             {
                 opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
@@ -67,7 +63,6 @@ namespace ChessWeb.Api
 
             app.UseCors();
             app.UseRouting();
-            app.UseFileServer();
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
